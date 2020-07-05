6003f579eade09a3d74f1a3203cef5d062e4e829
codeping <deping.du@gmail.com>
2020-07-06 01:20:35 +0800

    初始化 API  网关项目。

---
+++ MicroServices.Api/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServices.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string[] Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ MicroServices.Api/OcelotConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Ocelot.Configuration.File;
+
+namespace MicroServices.Api
+{
+    public class OcelotConfig
+    {
+        public static void Configuration(FileConfiguration config)
+        {
+            config.Routes.Add(new FileRoute
+            {
+                DownstreamPathTemplate = "/",
+                DownstreamScheme = "https",
+                DownstreamHostAndPorts = new List<FileHostAndPort>
+                {
+                    new FileHostAndPort
+                    {
+                        Host = "checkip.amazonaws.com",
+                        Port = 443,
+                    },
+                },
+
+                UpstreamPathTemplate = "/api/values",
+                UpstreamHttpMethod = new List<string>
+                {
+                    "Get",
+                },
+            });
+
+            config.GlobalConfiguration.BaseUrl = "http://localhost:5000";
+        }
+    }
+}
