13c1f428d83ba626ee8ec611039783d2972b75d1
codeping <deping.du@gmail.com>
2020-07-08 00:07:23 +0800

    添加客户端。

---
+++ src/MicroServices.Client/Api/IOrdersApi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MicroServices.Models;
+using WebApiClientCore;
+using WebApiClientCore.Attributes;
+
+namespace MicroServices.Client
+{
+    [LoggingFilter]
+    [HttpHost("http://localhost:8080")]
+    public interface IOrdersApi : IHttpApi
+    {
+        [HttpGet("/api/orders")]
+        Task<List<OrderInfo>> Get();
+
+        [HttpGet("/api/orders/{id}")]
+        Task<OrderInfo> GetById(long id);
+    }
+}
